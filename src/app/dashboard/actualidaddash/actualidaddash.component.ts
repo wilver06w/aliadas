83a0f1ca2355fec2ed857918438522d72bbcf2c3
Wilver Fabian Rojas Del Ducca <wilverfabian.06@gmail.com>
2024-05-31 22:06:59 -0500

    Changes required Number 5

--- src/app/dashboard/actualidaddash/actualidaddash.component.ts
+++ src/app/dashboard/actualidaddash/actualidaddash.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-actualidaddash',
   templateUrl: './actualidaddash.component.html',
-  styleUrls: ['./actualidaddash.component.css']
 })
 export class ActualidaddashComponent {
 
