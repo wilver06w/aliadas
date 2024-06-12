46f7a58409d1db347ef834d258bd84850f838762
Wilver Fabian Rojas Del Ducca <wilverfabian.06@gmail.com>
2024-06-11 22:04:19 -0500

    Feat: Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,19 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'appaliadas.aliadas.co',
-  appName: 'aliadas_front',
-  webDir: 'dist/aliadas_front',
-  server: {
-    androidScheme: 'https'
-  }
-,
-    android: {
-       buildOptions: {
-          keystorePath: 'c:\Users\Mauricio Murcia\Desktop\APP-ALIADAS\key.aliadas.jks',
-          keystoreAlias: 'key0',
-       }
-    }
-  };
+  appId: 'com.example.app',
+  appName: 'aliadas-front',
+  webDir: 'dist'
+};
 
 export default config;
